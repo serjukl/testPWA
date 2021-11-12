7f52cfa6bc239bcf9fff0c08ebd256edcc57beba
serjukl <serjukl98@gmail.com>
2021-11-12 22:36:40 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React , {useEffect} from 'react';
+import './App.sass';
+import Iframe from 'react-iframe'
+
+const App = () => {
+  let deferredPrompt;
+
+  window.addEventListener('beforeinstallprompt', (e) => {
+      deferredPrompt = e;
+  });
+
+  const installHandler = async() => {
+    if (deferredPrompt !== null) {
+      deferredPrompt.prompt();
+      const { outcome } = await deferredPrompt.userChoice;
+      console.log(outcome)
+      if (outcome === 'accepted') {
+          deferredPrompt = null;
+      }
+    }
+  }
+
+  return (
+    <div className="App">
+      <button onClick={installHandler} className="installApp">install app</button>
+      <Iframe url="https://test10jklger.xyz/?start=index&domain=mbchainhrates.xyz&description=1111&cip=222&fbclid=123&GCLID=123&utm_source=123&utm_campaign=345&utm_content=678&pixel=test&ttq=test&term=test&ym=228"
+        className="iframeContainer"
+        />
+    </div>
+  )
+}
+
+export default App
