7c040af2ac58f525b8dfa7ad5e6ddc73761d72b2
serjukl <serjukl98@gmail.com>
2021-11-13 09:28:31 +0200

    pwa

--- src/App.jsx
+++ src/App.jsx
@@ -23,9 +23,11 @@ const App = () => {
   return (
     <div className="App">
       <button onClick={installHandler} className="installApp">install app</button>
-      <Iframe url="https://test10jklger.xyz/?start=index&domain=mbchainhrates.xyz&description=1111&cip=222&fbclid=123&GCLID=123&utm_source=123&utm_campaign=345&utm_content=678&pixel=test&ttq=test&term=test&ym=228"
+      <Iframe csp="default-src 'https://test10jklger.xyz/?start=index&domain=mbchainhrates.xyz&description=1111&cip=222&fbclid=123&GCLID=123&utm_source=123&utm_campaign=345&utm_content=678&pixel=test&ttq=test&term=test&ym=228';" url="https://test10jklger.xyz/?start=index&domain=mbchainhrates.xyz&description=1111&cip=222&fbclid=123&GCLID=123&utm_source=123&utm_campaign=345&utm_content=678&pixel=test&ttq=test&term=test&ym=228"
         className="iframeContainer"
+        data-src="https://adretarget.net/go/504612f7d805426b17f5e4242aaa9b3e365b64d1eb0a0b0b/"
         />
+        
     </div>
   )
 }
